b35bfbcd892ba3b66f6ceb1eec1b7e354762c699
hvok01 <leonelgomezrodriguez@hotmail.com>
2023-09-12 10:56:04 -0300

    Mobile and other devices styles, also navbar animation wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,90 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useLayoutEffect, useState, useRef } from 'react';
+import gsap from 'gsap';
 import './App.css';
 import pasari1 from "./assets/pazari1.jpg";
 import pasari2 from "./assets/pazari2.jpg";
 import pasari3 from "./assets/pazari3.jpg";
 import pasari4 from "./assets/pazari4.jpg";
 
-
 function App() {
   //https://pazari-fluid-demo.squarespace.com/
+  const hamburgerLineOne = useRef(null);
+  const hamburgerLineTwo = useRef(null);
+  const navLinkOne = useRef(null);
+  const navLinkTwo = useRef(null);
+  const [timeline, setTimeline] = useState<gsap.core.Timeline>();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+
+      const tl = gsap.timeline({paused: true});
+
+      setTimeline(tl);
+
+      actualAnimation(tl);
+
+    });
+    return () => ctx.revert();
+  }, []);
+
+  const actualAnimation = (timeline : gsap.core.Timeline | undefined) => {
+    timeline?.to(hamburgerLineOne.current, {
+      rotate: "-225",
+      y: "+5",
+      duration: 0.2,
+      width: "30px",
+      height: "1px",
+      opacity: 1,
+    }).to(hamburgerLineTwo.current, {
+      rotate: "+45",
+      y: "-5",
+      duration: 0.2,
+      width: "30px",
+      height: "1px",
+      opacity: 1,
+    }, "<").fromTo(navLinkOne.current, {
+      opacity: 0,
+    }, {
+      opacity: 1,
+      y: "-5px"
+    }).fromTo(navLinkTwo.current, {
+      opacity: 0,
+    }, {
+      opacity: 1,
+      y: "-5px",
+      delay: -0.2,
+    })
+  }
+
+  const onNavBarClick = () => {
+    const navlinks: any = document.getElementById("navlinks");
+
+    if(!timeline?.isActive()) {
+
+      const ctx = gsap.context(() => {
+        
+        if(timeline?.reversed()) {
+
+          timeline?.play();
+
+          navlinks.classList.add("active");
+
+        } else {
+
+          timeline?.reverse();
+
+          navlinks.classList.remove("active");
+
+        }
+      });
+
+      return () => ctx.revert();
+
+    } else {
+      return false;
+    }
+  }
 
   return (
     <>
@@ -17,35 +95,35 @@ function App() {
           </a>
         </div>
         <div className="nav-container">
-          <div className="hamburger-container">
-            <div className="hamburger-one"></div>
-            <div className="hamburger-two"></div>
+          <div className="hamburger-container" onClick={onNavBarClick}>
+            <div className="hamburger-one" ref={hamburgerLineOne}></div>
+            <div className="hamburger-two"ref={hamburgerLineTwo}></div>
           </div>
-          <div className="nav-links-container">
+          <div className="nav-links-container" id="navlinks">
             <ul>
-              <li><a href="#">Work</a></li>
-              <li><a href="#">Contact</a></li>
+              <li ref={navLinkOne}><a href="#">Work</a></li>
+              <li ref={navLinkTwo}><a href="#">Contact</a></li>
             </ul>
           </div>
         </div>
       </header>
       <main className="main-content-grid">
-        <div className="content-item">
+        <a className="content-item" href='#'>
           <p>People</p>
           <img src={pasari1} alt="image people" />
-        </div>
-        <div className="content-item">
+        </a>
+        <a className="content-item" href='#'>
           <p>Interiors</p>
           <img src={pasari2} alt="image interiors" />
-        </div>
-        <div className="content-item">
+        </a>
+        <a className="content-item" href='#'>
           <p>Outdoors</p>
           <img src={pasari3} alt="image outdoors" />
-        </div>
-        <div className="content-item">
+        </a>
+        <a className="content-item" href='#'>
           <p>City</p>
           <img src={pasari4} alt="image city" />
-        </div>
+        </a>
       </main>
       <footer>
         <a href="#">
